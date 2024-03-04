9d8ef5ccafbd2590b8530396ad0669da2b696207
Kanad Shee <codewizardkanad@gmail.com>
2024-03-04 15:59:06 +0530

    Added one more file

---
+++ DSA/DSA/06_Arrays/Easy/06_Left_Rotate_Array_by_One_place.cpp
@@ -0,0 +1,26 @@
+/// Left rotate array by one place:
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Approach:
+/*
+1. Take the first element in a temp variable
+2. Move the other elements to te left by one place (arr[i-1] = arr[i])
+3. Again place the temp element as the last element of array
+*/
+vector<int> leftRotateArrayByOnePlace (vector<int> arr){
+    int n = arr.size();
+    int temp = arr[0];
+    for (int i = 1; i < n; i ++){
+        arr[i-1] =arr[i];
+    }
+    arr[n-1] = temp;
+    return arr;
+} 
+
+
+int main() {
+    vector<int> arr = {1, 20, 41, 87, 41, 31};
+    return 0;
+}
