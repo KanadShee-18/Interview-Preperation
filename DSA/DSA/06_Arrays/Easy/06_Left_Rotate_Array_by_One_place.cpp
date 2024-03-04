64f38494e7afcf3fecddea22b925289f7a3cf2e0
Kanad Shee <codewizardkanad@gmail.com>
2024-03-04 16:02:32 +0530

    Added more

--- DSA/DSA/06_Arrays/Easy/06_Left_Rotate_Array_by_One_place.cpp
+++ DSA/DSA/06_Arrays/Easy/06_Left_Rotate_Array_by_One_place.cpp
@@ -9,7 +9,7 @@ using namespace std;
 2. Move the other elements to te left by one place (arr[i-1] = arr[i])
 3. Again place the temp element as the last element of array
 */
-vector<int> leftRotateArrayByOnePlace (vector<int> arr){
+vector<int> leftRotateArrayByOnePlace (vector<int> &arr){
     int n = arr.size();
     int temp = arr[0];
     for (int i = 1; i < n; i ++){
@@ -22,5 +22,14 @@ vector<int> leftRotateArrayByOnePlace (vector<int> arr){
 
 int main() {
     vector<int> arr = {1, 20, 41, 87, 41, 31};
+    cout << "Before left rotating array by one place the array is: ";
+    for (int i = 0; i < arr.size(); i ++){
+        cout << arr[i] << " ";
+    }
+    leftRotateArrayByOnePlace(arr);
+    cout << endl << "After left rotating array by one place the array is looking like: ";
+    for (int i = 0; i < arr.size(); i ++){
+        cout << arr[i] << " ";
+    }
     return 0;
 }
