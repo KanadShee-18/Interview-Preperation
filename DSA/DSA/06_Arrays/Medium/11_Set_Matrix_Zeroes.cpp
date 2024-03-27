8d5c5c14253f8afd90d4dd707db6ecfa66fa6a21
Kanad Shee <codewizardkanad@gmail.com>
2024-03-27 18:24:16 +0530

    Added questions to files

--- DSA/DSA/06_Arrays/Medium/11_Set_Matrix_Zeroes.cpp
+++ DSA/DSA/06_Arrays/Medium/11_Set_Matrix_Zeroes.cpp
@@ -1,1 +1,8 @@
 //// Set Matrix Zeroes:
+
+// Link: https://leetcode.com/problems/set-matrix-zeroes/description/
+
+/*
+Input: matrix = [[1,1,1],[1,0,1],[1,1,1]]
+Output: [[1,0,1],[0,0,0],[1,0,1]]
+*/
