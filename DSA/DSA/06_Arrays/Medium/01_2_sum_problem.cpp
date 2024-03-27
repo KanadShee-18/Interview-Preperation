78934bd44e782a829ee6cd9d2433c5ed1d378cbd
Kanad Shee <codewizardkanad@gmail.com>
2024-03-27 19:35:31 +0530

    Solved syntactical error

--- DSA/DSA/06_Arrays/Medium/01_2_sum_problem.cpp
+++ DSA/DSA/06_Arrays/Medium/01_2_sum_problem.cpp
@@ -94,11 +94,17 @@ bool two_Sum(vector<int> &arr, int target)
     {
         int sum = arr[left] + arr[right];
         if (sum == target)
+        {
             return true;
+        }
         else if (sum < target)
+        {
             left++;
+        }
         else
+        {
             right--;
+        }
     }
     return false;
 }
