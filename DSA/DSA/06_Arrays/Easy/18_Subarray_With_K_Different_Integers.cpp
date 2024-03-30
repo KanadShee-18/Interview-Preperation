0cf5d64e091d0f0c18a364fecf6e32e38aee6bb5
Kanad Shee <codewizardkanad@gmail.com>
2024-03-30 12:17:13 +0530

    Added 18 no problem link

--- DSA/DSA/06_Arrays/Easy/18_Subarray_With_K_Different_Integers.cpp
+++ DSA/DSA/06_Arrays/Easy/18_Subarray_With_K_Different_Integers.cpp
@@ -1,5 +1,6 @@
 //// Subarrays with K different integers:
 //  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Link: https://leetcode.com/problems/subarrays-with-k-different-integers/description/
 /*
 Given an integer array nums and an integer k, return the number of good subarrays of nums.
 A good array is an array where the number of different integers in that array is exactly k.
